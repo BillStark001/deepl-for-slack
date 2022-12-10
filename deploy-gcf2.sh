c09ea8a5141d7d22420acd1446582699a5e6b216
Bill Stark <31788509+BillStark001@users.noreply.github.com>
2022-12-11 03:59:07 +0900

    Update deploy-gcf2.sh

--- deploy-gcf2.sh
+++ deploy-gcf2.sh
@@ -1,8 +1,8 @@
 gcloud functions deploy nodejs-http-function \
 --gen2 \
---runtime=nodejs16 \
---region=asia-northeast-1 \
---source=./src/lib \
---entry-point=helloGET \
+--runtime=nodejs18 \
+--region=asia-northeast1 \
+--source=./lib \
+--entry-point=deepLSlackBot \
 --trigger-http \
 --allow-unauthenticated
